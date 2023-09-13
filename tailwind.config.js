184cb6668e56c3e88b6ca21761e437bf4b607529
Moises Navarro <MoisesINMDeveloper@hotmail.com>
2023-09-13 12:50:40 -0400

    Actualizando archivo tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,png,jpg}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
       montserrat: "Montserrat",
       fraunces: "Fraunces",
-    },
-    backgroundImage: {
-      "mobile-img": "url('/img/hero-pattern.svg')",
-      "desktop-img": "url('/img/footer-texture.png')",
     },
     extend: {},
   },
